66e24a23ff09fcc3ec04d2e87965f297f274a062
RudkoMaxim <70272922+RudkoMaxim@users.noreply.github.com>
2024-04-03 13:18:02 +0300

    Add files via upload

---
+++ SecondClass.java
@@ -0,0 +1,7 @@
+package org.example.task_8;
+
+public class SecondClass extends ThirdClass{
+    public SecondClass () {
+        super("primer");
+    }
+}
---
+++ ThirdClass.java
@@ -0,0 +1,9 @@
+package org.example.task_8;
+
+public class ThirdClass {
+    private String peremennaja;
+
+    public ThirdClass (String stroka){
+        this.peremennaja = stroka;
+    }
+}
